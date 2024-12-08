7b2d8a04d1d63b5cc843bec168c84e7b3c9aa399
Hoang381202 <phuonglien3802@gmail.com>
2024-12-08 18:46:44 +0700

    Add files via upload

---
+++ nguhub.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Test",--cutchanhub
+    logoscript =   "121838630736220"
+    tacgia = "sadas"-- Vương Hoàng
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
